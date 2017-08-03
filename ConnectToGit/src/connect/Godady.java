a4ce9296ce582d6405492a908727667ea19d2b25
PTBLR-1193 <PTBLR-1193@PTBLR-1193-PC>
2017-08-03 15:21:36 +0530

    Changed code

--- ConnectToGit/src/connect/Godady.java
+++ ConnectToGit/src/connect/Godady.java
@@ -1,9 +1,16 @@
 package connect;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Godady {
+	
+	
+	 public static WebDriver driver;
 	public static void main (String []args){
-		System.out.println("Testing")
-		
+		System.setProperty("webdriver.chrome.driver",	"C:\\Users\\PTBLR-1193\\Downloads\\Firefox\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("google.co.in");
 	}
 
 }
